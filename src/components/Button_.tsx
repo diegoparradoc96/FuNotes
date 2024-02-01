48fcfd9e6a092ff47d02365e3036162a0934cfd0
diegoparradoc96 <diegoparrado.cardozo@hotmail.com>
2024-02-01 08:24:11 -0500

    first commit

---
+++ src/components/Button_.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Button_ = () => {
+  return (
+    <View>
+      <Text>Button_</Text>
+    </View>
+  );
+};
+
+export {Button_};
---
+++ src/views/Home_.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home_ = () => {
+  return (
+    <View>
+      <Text>Home_</Text>
+    </View>
+  );
+};
+
+export {Home_};
